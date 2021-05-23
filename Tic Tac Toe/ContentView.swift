9ec56f536758f92eec9b34a2a8fc923682b927c9
fitchatron <53107122+fitchatron@users.noreply.github.com>
2021-05-23 14:47:21 +1000

    initial commit

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -3,14 +3,40 @@
 //  Tic Tac Toe
 //
 //  Created by James Fitch on 23/05/2021.
-//
+// can make smarter using https://www.geeksforgeeks.org/minimax-algorithm-in-game-theory-set-3-tic-tac-toe-ai-finding-optimal-move/amp/
 
 import SwiftUI
 
 struct ContentView: View {
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
+    
     var body: some View {
-        Text("Hello, world!")
+        GeometryReader { geomerty in
+            VStack {
+                Spacer()
+                
+                LazyVGrid(columns: columns, spacing: 5) {
+                    ForEach(0..<9) { item in
+                        ZStack {
+                            Circle()
+                                .foregroundColor(.red)
+                                .opacity(0.5)
+                                .frame(width: geomerty.size.width / 3 - 15, height: geomerty.size.width / 3 - 15)
+                            
+                            Image(systemName: "xmark")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                                .foregroundColor(.white)
+                        }
+                    }
+                }
+                
+                Spacer()
+            }
             .padding()
+        }
     }
 }
 
