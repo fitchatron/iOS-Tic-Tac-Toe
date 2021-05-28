c8a5ebe7469c77065ac5c94cded34de7cb262eeb
fitchatron <53107122+fitchatron@users.noreply.github.com>
2021-05-28 23:12:25 +1000

    Update ContentView.swift

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -12,6 +12,9 @@ struct ContentView: View {
                                GridItem(.flexible()),
                                GridItem(.flexible())]
     
+    @State private var moves: [Move?] = Array(repeating: nil, count: 9)
+    @State private var isGameBoardDisabled = false
+    
     var body: some View {
         GeometryReader { geomerty in
             VStack {
@@ -25,19 +28,91 @@ struct ContentView: View {
                                 .opacity(0.5)
                                 .frame(width: geomerty.size.width / 3 - 15, height: geomerty.size.width / 3 - 15)
                             
-                            Image(systemName: "xmark")
+                            Image(systemName: moves[item]?.indicator ?? "")
                                 .resizable()
                                 .frame(width: 40, height: 40)
                                 .foregroundColor(.white)
                         }
+                        .onTapGesture {
+                            if (isSquareOccupied(in: moves, forIndex: item)) { return }
+                            moves[item] = Move(player: .human, boardIndex: item)
+                            isGameBoardDisabled = true
+                            
+                            if checkWinCondition(for: .human, in: moves) {
+                                print("human Wins")
+                                return
+                            }
+                            
+                            if checkForDraw(in: moves) {
+                                print("draw")
+                                return
+                            }
+                            
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                let computerPosition = determineComputerMovePosition(in: moves)
+                                moves[computerPosition] = Move(player: .computer, boardIndex: computerPosition)
+                                
+                                if checkWinCondition(for: .computer, in: moves) {
+                                    print("computer Wins")
+                                    return
+                                }
+                                
+                                if checkForDraw(in: moves) {
+                                    print("draw")
+                                    return
+                                }
+                                
+                                isGameBoardDisabled = false
+                            }
+                        }
                     }
                 }
-                
                 Spacer()
             }
+            .disabled(isGameBoardDisabled)
             .padding()
         }
     }
+
+    func isSquareOccupied(in moves: [Move?], forIndex index: Int) -> Bool {
+        moves.contains(where: {$0?.boardIndex == index})
+    }
+    
+    func determineComputerMovePosition(in moves: [Move?]) -> Int {
+        var movePosition = Int.random(in: 0..<9)
+        
+        while isSquareOccupied(in: moves, forIndex: movePosition) {
+            movePosition = Int.random(in: 0..<9)
+        }
+        
+        return movePosition
+    }
+    
+    func checkWinCondition(for player: Player, in moves: [Move?]) -> Bool {
+        let winPatterns: Set<Set<Int>> = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+        
+        let playerMoves = moves.compactMap { $0 }.filter { $0.player == player }
+        let playerPositions = Set(playerMoves.map{ $0.boardIndex})
+        
+        for pattern in winPatterns where pattern.isSubset(of: playerPositions) { return true }
+    
+        return false
+    }
+    
+    func checkForDraw(in moves: [Move?]) -> Bool {
+        return moves.compactMap { $0 }.count == 9
+    }
+}
+
+enum Player {
+    case human, computer
+}
+
+struct Move {
+    let player: Player
+    let boardIndex: Int
+    
+    var indicator: String { player == .human ? "xmark" : "circle" }
 }
 
 struct ContentView_Previews: PreviewProvider {
